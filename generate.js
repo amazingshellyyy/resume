ce8c47b3d50824c862afeec14fc9a390fc31b96d
amazingshellyyy <amazingshellyyy@gmail.com>
2020-04-16 14:14:58 -0700

    remove

--- generate.js
+++
@@ -1,7 +0,0 @@
-const marked = require('marked');
-const fs = require('fs');
-
-const readMe = fs.readFileSync('README.md', 'utf-8');
-const markdownReadMe = marked(readMe);
-
-fs.writeFileSync('./README.html', markdownReadMe);
